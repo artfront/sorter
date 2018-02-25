a7b50835b967cad50a9a63cc62a5fdf794f9544b
artfront <krivonosart19@gmail.com>
2018-02-25 23:50:06 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.store = [];
+        this.comparator = (a, b) => {return a - b};
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.store.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.store[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.store.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.store;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort((a, b) => {
+            if(a < b) {
+            if(this.comparator(this.store[a], this.store[b]) > 0) {
+                const tmp = this.store[b];
+                this.store[b] = this.store[a];
+                this.store[a] = tmp;
+            }
+        } else {
+            if(this.comparator(this.store[a], this.store[b]) < 0) {
+            const tmp = this.store[b];
+            this.store[b] = this.store[a];
+            this.store[a] = tmp;
+            }
+        }
+        return b - a;
+    });
+    }
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
 }
-
 module.exports = Sorter;
+
